c7ddb927d6b092e0eb2f051367d8686d225a3553
Gustavo Valentim <gustavosvalentim1@gmail.com>
2023-01-13 12:53:01 -0300

    chore: added a few comments explaining code

--- pulumi-modules/index.ts
+++ pulumi-modules/index.ts
@@ -8,70 +8,89 @@ const resourceNamePrefix = config.require("resourceNamePrefix");
 const resourceNameSuffix = config.require("resourceNameSuffix");
 
 function getResourceName(name: string) {
-    return `${resourceNamePrefix}-${name}-${resourceNameSuffix}`;
+  return `${resourceNamePrefix}-${name}-${resourceNameSuffix}`;
 }
 
 // Create an Azure Resource Group
 const resourceGroup = new resources.ResourceGroup(getResourceName("rg"));
 
 // Create App Service Plan
-const appServicePlan = new web.AppServicePlan(getResourceName("appplan"), {
+const appServicePlan = new web.AppServicePlan(
+  getResourceName("appplan"),
+  {
     resourceGroupName: resourceGroup.name,
-    kind: "linux",
-    sku: {
-        name: config.require("skuName"),
-        tier: config.require("skuTier"),
-    },
-    reserved: true,
-}, { parent: resourceGroup, dependsOn: [resourceGroup] });
 
-const appInsights = new insights.Component(getResourceName("insights"), {
-    resourceGroupName: resourceGroup.name,
+    // Pricing
+    sku: {
+      name: config.require("skuName"),
+      tier: config.require("skuTier"),
+    },
+
+    // Linux environment
     kind: "linux",
-    applicationType: insights.ApplicationType.Web
-}, { dependsOn: [resourceGroup], parent: resourceGroup });
+    reserved: true,
+
+    // Scaling
+    perSiteScaling: true,
+  },
+  { parent: resourceGroup, dependsOn: [resourceGroup] }
+);
+
+const appInsights = new insights.Component(
+  getResourceName("insights"),
+  {
+    resourceGroupName: resourceGroup.name,
+    kind: "web",
+    applicationType: insights.ApplicationType.Web,
+  },
+  { dependsOn: [resourceGroup], parent: resourceGroup }
+);
 
 // Create App Service
 const appServiceName = getResourceName("api");
-const appService = new web.WebApp(appServiceName, {
+const appService = new web.WebApp(
+  appServiceName,
+  {
     resourceGroupName: resourceGroup.name,
     serverFarmId: appServicePlan.id,
     httpsOnly: true,
     name: appServiceName,
     siteConfig: {
-        healthCheckPath: "/health",
-        nodeVersion: "16-lts",
-        loadBalancing: web.SiteLoadBalancing.WeightedRoundRobin,
-        limits: {
-            maxPercentageCpu: 20
+      healthCheckPath: "/health",
+      nodeVersion: "16-lts",
+      loadBalancing: web.SiteLoadBalancing.WeightedRoundRobin,
+      limits: {
+        maxPercentageCpu: 20,
+      },
+      alwaysOn: true,
+      httpLoggingEnabled: true,
+      detailedErrorLoggingEnabled: true,
+      appSettings: [
+        {
+          name: "API_ENVIRONMENT",
+          value: "dev",
         },
-        alwaysOn: true,
-        httpLoggingEnabled: true,
-        detailedErrorLoggingEnabled: true,
-        appSettings: [
-            {
-                name: "API_ENVIRONMENT",
-                value: "dev"
-            },
-            {
-                name: "APPINSIGHTS_INSTRUMENTATIONKEY",
-                value: appInsights.instrumentationKey,
-            },
-            {
-                name: "APPLICATIONINSIGHTS_CONNECTION_STRING",
-                value: pulumi.interpolate`InstrumentationKey=${appInsights.instrumentationKey}`,
-            },
-            {
-                name: "ApplicationInsightsAgent_EXTENSION_VERSION",
-                value: "~2",
-            },
-            {
-                name: "SCM_DO_BUILD_DURING_DEPLOYMENT",
-                value: "true",
-            },
-        ],
+        {
+          name: "APPINSIGHTS_INSTRUMENTATIONKEY",
+          value: appInsights.instrumentationKey,
+        },
+        {
+          name: "APPLICATIONINSIGHTS_CONNECTION_STRING",
+          value: pulumi.interpolate`InstrumentationKey=${appInsights.instrumentationKey}`,
+        },
+        {
+          name: "ApplicationInsightsAgent_EXTENSION_VERSION",
+          value: "~2",
+        },
+        {
+          name: "SCM_DO_BUILD_DURING_DEPLOYMENT",
+          value: "true",
+        },
+      ],
     },
-}, { parent: appServicePlan, dependsOn: [appServicePlan] });
+  },
+  { parent: appServicePlan, dependsOn: [appServicePlan] }
+);
 
 export const AppServiceName = appService.name;
 export const WebApp = pulumi.interpolate`https://${appService.defaultHostName}`;
